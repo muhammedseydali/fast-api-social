5488b29478b40213c28a634b1acca22df49f9c94
muhammedseydali <muhammedseydali002@gmail.com>
2025-08-30 12:37:42 +0530

    main.py updated

--- app/main.py
+++ app/main.py
@@ -20,5 +20,5 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
-app.include_router(auth.routers)
+app.include_router(auth.router)
 app.include_router(posts.router)
